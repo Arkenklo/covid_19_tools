7671b8c835cb302be418d06168b56b2a0a7e14ce
Robert Arkenklo <robert@mrfriday.com>
2020-03-21 21:16:58 +0100

    Non-funtional checkin

--- run.sh
+++ run.sh
@@ -3,15 +3,21 @@
 # Quit this script if any command fails
 set -e
 
+# Setup virtualenv and install requirements
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+
+# Do some Linux-only cheats to get chromedriver to work
+cd env/
+ln -s chromedriver-Linux64 chromedriver || true # || true is to ignore errors if link exists
+cd ..
+PATH=$PATH:./env/
+
 # Convert notebooks to python
 jupyter nbconvert website_updates.ipynb --to python
 jupyter nbconvert nhs_advice.ipynb --to python
 
-# Setup virtualenv and install requirements
-python3 -m venv env
-source env/bin/activate
-pip install -r requirements.txt
-
 # Run the scripts
-python3 ./website_updates.py
+#python3 ./website_updates.py
 python3 ./nhs_advice.py
